9a8a1c0834c5bda8e5aca18acf54d0a7269ff713
laurentiue-sj <laurentiu@solutionjunkies.co.uk>
2023-04-11 10:20:13 +0100

    Added logic for hero banner configurable background image in static resources

--- force-app/main/default/lwc/nhsHeroBanner/nhsHeroBanner.js
+++ force-app/main/default/lwc/nhsHeroBanner/nhsHeroBanner.js
@@ -5,8 +5,14 @@
  **/
 import {LightningElement, api, track} from 'lwc';
 import {NavigationMixin} from "lightning/navigation";
-
+import HeroImg from '@salesforce/resourceUrl/HeroImg';
 export default class nhsHeroBanner extends NavigationMixin(LightningElement) {
     @api heroDescription = "Hero Description";
     @api heroTitle = "Hero Title";
+
+    //Background image stored in static resources for the Hero Banner
+    imageUrl = HeroImg;
+    get getHeroImg(){
+        return `background-image:url("${this.imageUrl}")`;
+    }
 }
